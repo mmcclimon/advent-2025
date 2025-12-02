2e8c415a8c3fcc0f846ad25417d16db5201ee5be
Michael McClimon <michael@mcclimon.org>
2025-12-02 09:26:51 -0500

    day 2: clean up to make it less embarrassing

--- day02/main.go
+++ day02/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"slices"
 	"strings"
 
 	"github.com/mmcclimon/advent-2025/advent/assert"
@@ -19,36 +18,35 @@ func main() {
 		nums := strings.Split(strings.TrimSpace(chunk), "-")
 		left, right := nums[0], nums[1]
 
-		for i := conv.Atoi(left); i <= conv.Atoi(right); i++ {
-			s := fmt.Sprint(i)
-
-			mid := len(s) / 2
-
-			var ok []int
+		for num := conv.Atoi(left); num <= conv.Atoi(right); num++ {
+			s := fmt.Sprint(num)
+			var good int
 
 		Chunk:
-			for size := 1; size <= mid; size++ {
-				if len(s)%size != 0 {
+			for size := len(s) / 2; size >= 1; size-- {
+				chunks := split(s, size)
+				numChunks := len(chunks)
+				if numChunks == 0 {
 					continue
 				}
 
-				chunked := split(s, size)
-
-				for j := 1; j < len(chunked); j++ {
-					if chunked[j] != chunked[0] {
+				for i := 1; i < numChunks; i++ {
+					if chunks[i] != chunks[0] {
 						continue Chunk
 					}
 				}
 
-				ok = append(ok, len(chunked))
+				good = numChunks
+				break
 			}
 
-			if len(ok) > 0 {
-				part2 += i
-			}
-
-			if slices.Contains(ok, 2) {
-				part1 += i
+			switch good {
+			case 0:
+			case 2:
+				part1 += num
+				fallthrough
+			default:
+				part2 += num
 			}
 		}
 	}
@@ -58,7 +56,9 @@ func main() {
 }
 
 func split(s string, size int) []string {
-	assert.True(len(s)%size == 0, "string must divide evenly by size")
+	if len(s)%size != 0 {
+		return nil
+	}
 
 	var ret []string
 	var cur string
@@ -72,8 +72,5 @@ func split(s string, size int) []string {
 	}
 
 	assert.True(cur == "", "ended up with extra chars??")
-
 	return ret
 }
-
-// 11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124
