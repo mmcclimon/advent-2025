6c13a6a08038aaf074f3e9ed228f4765092175da
Michael McClimon <michael@mcclimon.org>
2025-12-02 20:42:14 -0500

    day 2: speed it up and make it less weird
    We can just do this by construction rather than splitting all the
    strings and checking.

--- day02/main.go
+++ day02/main.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"strings"
 
-	"github.com/mmcclimon/advent-2025/advent/assert"
 	"github.com/mmcclimon/advent-2025/advent/conv"
 	"github.com/mmcclimon/advent-2025/advent/input"
 )
@@ -20,57 +19,22 @@ func main() {
 
 		for num := conv.Atoi(left); num <= conv.Atoi(right); num++ {
 			s := fmt.Sprint(num)
-			var good int
 
-		Chunk:
 			for size := len(s) / 2; size >= 1; size-- {
-				chunks := split(s, size)
-				numChunks := len(chunks)
-				if numChunks == 0 {
+				numChunks := len(s) / size
+				if strings.Repeat(s[0:size], numChunks) != s {
 					continue
 				}
 
-				for i := 1; i < numChunks; i++ {
-					if chunks[i] != chunks[0] {
-						continue Chunk
-					}
+				if numChunks == 2 {
+					part1 += num
 				}
-
-				good = numChunks
-				break
-			}
-
-			switch good {
-			case 0:
-			case 2:
-				part1 += num
-				fallthrough
-			default:
 				part2 += num
+				break
 			}
 		}
 	}
 
 	fmt.Println("part 1:", part1)
 	fmt.Println("part 2:", part2)
-}
-
-func split(s string, size int) []string {
-	if len(s)%size != 0 {
-		return nil
-	}
-
-	var ret []string
-	var cur string
-
-	for _, c := range s {
-		cur += string(c)
-		if len(cur) == size {
-			ret = append(ret, cur)
-			cur = ""
-		}
-	}
-
-	assert.True(cur == "", "ended up with extra chars??")
-	return ret
 }
