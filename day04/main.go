4dca564dcd750a245b6c3dde8548de4f8d7ffdd5
Michael McClimon <michael@mcclimon.org>
2025-12-04 08:29:09 -0500

    day 4: our first lil grid problem
    This was easier than I thought it would be.

---
+++ day04/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mmcclimon/advent-2025/advent/input"
+)
+
+type Grid = map[rc]struct{}
+type rc struct{ r, c int }
+
+func main() {
+	grid := make(Grid)
+	for r, line := range input.New().EnumerateLines() {
+		for c, char := range line {
+			if char == '@' {
+				grid[rc{r, c}] = struct{}{}
+			}
+		}
+	}
+
+	toRemove := removableRolls(grid)
+	fmt.Println("part 1:", len(toRemove))
+
+	part2 := 0
+
+	for len(toRemove) > 0 {
+		part2 += len(toRemove)
+
+		for _, pos := range toRemove {
+			delete(grid, pos)
+		}
+
+		toRemove = removableRolls(grid)
+	}
+
+	fmt.Println("part 2:", part2)
+}
+
+func removableRolls(grid Grid) []rc {
+	var ret []rc
+	for pos := range grid {
+		neighbors := neighborsFor(grid, pos)
+		if len(neighbors) < 4 {
+			ret = append(ret, pos)
+		}
+	}
+
+	return ret
+}
+
+// returns a list of positions occupied in the grid
+func neighborsFor(grid Grid, pos rc) []rc {
+	ret := make([]rc, 0, 8)
+
+	for _, r := range []int{pos.r - 1, pos.r, pos.r + 1} {
+		for _, c := range []int{pos.c - 1, pos.c, pos.c + 1} {
+			check := rc{r, c}
+			_, ok := grid[check]
+			if ok && check != pos {
+				ret = append(ret, check)
+			}
+		}
+	}
+
+	return ret
+}
