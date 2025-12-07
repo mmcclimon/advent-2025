5ac25f8de506d6e3692589e20b441adb1441a0f3
Michael McClimon <michael@mcclimon.org>
2025-12-07 12:57:42 -0500

    day 7: solve part 2
    This required a decent amount of hinting from the reddit.

--- day07/main.go
+++ day07/main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"fmt"
 
-	"github.com/mmcclimon/advent-2025/advent/collections"
 	"github.com/mmcclimon/advent-2025/advent/input"
 )
 
+const (
+	Blank    = 0
+	Splitter = -1
+)
+
 type Grid struct {
-	g    map[rc]rune
+	g    map[rc]int
 	maxR int
 	maxC int
 }
@@ -16,71 +20,92 @@ type Grid struct {
 type rc struct{ r, c int }
 
 func main() {
-	grid := Grid{g: make(map[rc]rune)}
+	grid := Grid{g: make(map[rc]int)}
 
 	for r, line := range input.New().EnumerateLines() {
 		for c, char := range line {
-			grid.g[rc{r, c}] = char
+			var n int
+			switch char {
+			case 'S':
+				n = 1
+			case '^':
+				n = Splitter
+			}
+
+			grid.g[rc{r, c}] = n
 			grid.maxC = c
 		}
 
 		grid.maxR = r
 	}
 
-	numSplits := 0
+	numSplits, numPaths := 0, 0
 
+	// Every position in the grid track the number of paths to that position. As
+	// we go, we add up the number of splitters we hit (for part 1), and then at
+	// the end we sum up all the paths to the bottom.
 	for r := range grid.maxR + 1 {
-		nextLine := collections.NewSet[rc]()
-
 		for c := range grid.maxC + 1 {
-			switch char := grid.Get(r, c); char {
-			case '.':
-				// nothing to do
-			case 'S', '|':
-				nextLine.Add(rc{r + 1, c})
-			case '^':
-				if grid.Get(r-1, c) != '|' {
-					// this splitter only splits if hit by a beam
-					continue
-				}
+			incoming := grid.Get(r-1, c)
 
-				numSplits++
-				grid.AddBeam(r, c-1)
-				grid.AddBeam(r, c+1)
-				nextLine.Add(rc{r + 1, c - 1}, rc{r + 1, c + 1})
+			// If there's nothing coming into this position, it's not interesting.
+			if incoming <= 0 {
+				continue
 			}
-		}
 
-		for next := range nextLine.Iter() {
-			grid.AddBeam(next.r, next.c)
+			if grid.Get(r, c) == Splitter {
+				// Each splitter splits the beam into its left and right
+				numSplits++
+				grid.AddBeam(r, c-1, incoming)
+				grid.AddBeam(r, c+1, incoming)
+			} else {
+				grid.AddBeam(r, c, incoming)
+			}
 		}
 	}
 
+	// Now, sum up the number of paths.
+	for c := range grid.maxC + 1 {
+		numPaths += grid.Get(grid.maxR, c)
+	}
+
 	fmt.Println("part 1:", numSplits)
+	fmt.Println("part 2:", numPaths)
 }
 
 func (g Grid) Print() {
 	for r := range g.maxR + 1 {
 		for c := range g.maxC + 1 {
-			fmt.Print(string(g.g[rc{r, c}]))
+			switch item := g.Get(r, c); item {
+			case Blank:
+				fmt.Print(".")
+			case Splitter:
+				fmt.Print("^")
+			default:
+				if item < 16 {
+					fmt.Printf("%x", item)
+				} else {
+					fmt.Print("x")
+				}
+			}
 		}
 		fmt.Print("\n")
 	}
 }
 
-func (g Grid) Get(r, c int) rune {
+func (g Grid) Get(r, c int) int {
 	return g.g[rc{r, c}]
 }
 
-func (g Grid) AddBeam(r, c int) {
+func (g Grid) AddBeam(r, c int, howMuch int) {
 	if r < 0 || r > g.maxR || c < 0 || c > g.maxC {
 		return
 	}
 
 	pos := rc{r, c}
-	switch char := g.g[pos]; char {
-	case '^', 'S':
-	case '.', '|':
-		g.g[pos] = '|'
+	switch item := g.g[pos]; item {
+	case Splitter:
+	default:
+		g.g[pos] += howMuch
 	}
 }
