eb4461ad8661de142870db42204d538dc7fc83c1
Michael McClimon <michael@mcclimon.org>
2025-12-13 11:59:53 -0500

    day 9: make it work correctly and faster
    Thanks to the reddit for helping me find my stupid intersection logic
    bug.

--- day09/main.go
+++ day09/main.go
@@ -1,15 +1,21 @@
 package main
 
 import (
+	"cmp"
 	"fmt"
+	"os"
+	"slices"
 	"strings"
 
+	"github.com/mmcclimon/advent-2025/advent/assert"
 	"github.com/mmcclimon/advent-2025/advent/conv"
 	"github.com/mmcclimon/advent-2025/advent/input"
-	"github.com/mmcclimon/advent-2025/advent/mathx"
+	"github.com/mmcclimon/advent-2025/advent/operator"
 )
 
 type xy struct{ x, y int }
+type Rect struct{ left, right, top, bottom int }
+type Edge struct{ left, right, top, bottom int }
 
 func main() {
 	var points []xy
@@ -18,20 +24,87 @@ func main() {
 		points = append(points, xy{ints[0], ints[1]})
 	}
 
-	part1(points)
+	rects, edges := part1(points)
+	part2(rects, edges)
 }
 
-func part1(points []xy) {
-	var best int
-
+//nolint:unused
+func part1(points []xy) ([]Rect, []Edge) {
+	var rects []Rect
+	var edges []Edge
 	for i, src := range points {
+		// first, stash the edge since we're looping already and will need this later
+		idx := operator.CrummyTernary(i+1 == len(points), 0, i+1)
+		dst := points[idx]
+
+		edges = append(edges, Edge{
+			left:   min(src.x, dst.x),
+			right:  max(src.x, dst.x),
+			top:    min(src.y, dst.y),
+			bottom: max(src.y, dst.y),
+		})
+
+		// then, grab all possible rectangles
 		for j := i + 1; j < len(points); j++ {
 			dst := points[j]
 
-			square := (1 + mathx.Abs(dst.x-src.x)) * (1 + mathx.Abs(dst.y-src.y))
-			best = max(best, square)
+			rect := Rect{
+				left:   min(src.x, dst.x),
+				right:  max(src.x, dst.x),
+				top:    min(src.y, dst.y),
+				bottom: max(src.y, dst.y),
+			}
+
+			rects = append(rects, rect)
 		}
 	}
 
-	fmt.Println("part 1:", best)
+	slices.SortFunc(rects, func(a, b Rect) int {
+		return cmp.Compare(b.Area(), a.Area())
+	})
+
+	slices.SortFunc(edges, func(a, b Edge) int {
+		return cmp.Compare(b.Length(), a.Length())
+	})
+
+	fmt.Println("part 1:", rects[0].Area())
+	return rects, edges
+}
+
+func (rect Rect) Area() int {
+	return (1 + rect.right - rect.left) * (1 + rect.bottom - rect.top)
+}
+
+func (edge Edge) Length() int {
+	return (edge.right - edge.left) + (edge.bottom - edge.top)
+}
+
+func part2(rects []Rect, edges []Edge) {
+RECT:
+	for _, rect := range rects {
+		for _, edge := range edges {
+			if edge.left < rect.right && edge.right > rect.left &&
+				edge.top < rect.bottom && edge.bottom > rect.top {
+				continue RECT
+			}
+		}
+
+		fmt.Println("part 2:", rect.Area())
+		return
+	}
+}
+
+// nolint
+func printSVG(points []xy, filename string) {
+	f, err := os.Create(filename)
+	assert.Nil(err)
+	defer f.Close()
+
+	fmt.Fprintln(f, `<svg xmlns="http://www.w3.org/2000/svg">`)
+	fmt.Fprintln(f, `<polygon stroke="none" fill="red" points="`)
+	for _, point := range points {
+		fmt.Fprintf(f, "%d,%d\n", point.x, point.y)
+	}
+	fmt.Fprintln(f, `" />`)
+	fmt.Fprintln(f, `</svg>`)
 }
